cc206e733c89b92e27e2b2a91503f67ab0959546
Genialngash <ngangadanche@gmail.com>
2022-01-23 20:40:14 +0300

    - count the number of appeareances of  a word in the page function  - sorting the word apperances function  Listvies to show the words and the values

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+
+import 'package:collection/collection.dart';
 import 'package:chaleno/chaleno.dart';
 import 'package:flutter/material.dart';
 
@@ -34,14 +37,24 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String urlInput = '';
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+
+  LinkedHashMap<String, int> commonWords = LinkedHashMap();
+  Map<String, int> wordCount = {};
+
+  getCommonWords() {
+    //convert the values of the map to a list first
+    var sortedKeys = wordCount.keys.toList(growable: false)
+      //sort the list of values of the map in an ascending manner by starting with k2,k1
+      ..sort((k2, k1) => wordCount[k1]!.compareTo(wordCount[k2]!));
+    // The new value of  commonWords is map where the values are mapped to the respective keys
+    commonWords = LinkedHashMap.fromIterable(sortedKeys,
+        key: (k) => k, value: (k) => wordCount[k]!);
   }
 
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xff064DAE),
@@ -71,34 +84,65 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               ElevatedButton(
                 onPressed: () async {
-                 
+                  var symbols = "!@#\$%^&*()_-+={[}]|\\;:\"<>?/., ";
 
                   if (_formKey.currentState!.validate()) {
+                    //declaring the variables
+
+                    List words = [];
+                    //using the chaleno package to load the url
                     Chaleno().load(urlInput).then((value) {
-                      List<Result>? result = value?.getElementsByTagName('div');
+                      //get elements in the div tag
+                      List<Result>? result =
+                          value?.getElementsByTagName('body');
+                      // iterate through them to get the text
                       for (var item in result!) {
-                        print(item.text);
+                        String content = item.text.toString();
+                        words = content.toLowerCase().split(' ');
+                        wordCount = Map<String, int>.fromIterable(words,
+                            key: (item) => item.toString(), value: (item) => 0);
+                        for (var word in words) {
+                          if (words.contains(word)) {
+                            wordCount.update(word, (value) => value + 1);
+                          } else {
+                            wordCount.update(word, (value) => 1);
+                          }
+                        }
                       }
+                      // print('______---------------------------------________');
+                      // print(wordCount);
+                      // print('_____-------------------------------_________');
+                      getCommonWords();
+                      print(commonWords);
                     });
-                    
                   } else {
                     print('not valid');
                   }
                 },
                 child: const Text('Submit'),
               ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
+              commonWords.isEmpty
+                  ? SizedBox()
+                  : Container(
+                      height: screenHeight - 200,
+                      width: screenWidth - 10,
+                      child: ListView.builder(
+                        itemCount: commonWords.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          String key = commonWords.keys.elementAt(index);
+                          int value = commonWords.values.elementAt(index);
+                          return Wrap(children: [
+                            ListTile(
+                              title: Text(key),
+                              subtitle: Text("$value"),
+                            ),
+                          ]);
+                        },
+                      ),
+                    ),
             ],
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
